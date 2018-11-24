7db4ce247473dddbfcd0616a473c5fad1a7ed9cc
neko <neko.bj4501@gmail.com>
2018-11-24 15:54:36 +0800

    basic init

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+/*
+ * @Author: Jmeow
+ * @Date: 2018-11-24 11:26:37
+ * @Description: 
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+
+ReactDOM.render(
+  <div>
+    <i className="fa fa-exchange"></i>
+    <h1>Hello, world!</h1>
+  </div>,
+  document.getElementById('app')
+);
